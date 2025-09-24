54c1945bbd8a9413ae40d95fe2985f471efd2b71
AtharvaKadam27 <atharvak396@gmail.com>
2025-09-24 23:19:12 +0530

    Local Storage

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,7 +1,6 @@
 let input = document.querySelector(".main-input");
 let add = document.querySelector(".add");
 let todoContainer = document.querySelector(".todo-list-container");
-let todoList = [];
 
 add.addEventListener("click", () => {
   if (!input.value.trim()) return alert("Please add your task");
@@ -13,9 +12,10 @@ function createElement(value) {
   todo.classList.add("todo");
   let check = document.createElement("input");
   check.type = "checkbox";
+  check.checked = value.completed;
   todo.appendChild(check);
   let p = document.createElement("p");
-  p.innerText = value;
+  p.innerText = value.text;
   p.classList.add("text");
   todo.appendChild(p);
   let btn = document.createElement("button");
@@ -23,25 +23,49 @@ function createElement(value) {
   btn.classList.add("remove");
   todo.appendChild(btn);
   todo.addEventListener("click", function (e) {
+    console.log("remove");
+
     if (e.target.tagName === "P" || e.target.tagName === "INPUT") {
       this.children[1].classList.toggle("complete");
       this.children[0].checked = !this.children[0].checked;
+
+      saveTodo();
     }
 
     if (e.target.tagName === "BUTTON") {
-      if (this.children[0].checked) {
-        todoContainer.removeChild(this);
-      }
+      todoContainer.removeChild(this);
+      saveTodo();
     }
   });
-  todoList = [...todoList, { text: input.value, checked: check.checked }];
 
   todoContainer.appendChild(todo);
+  saveTodo();
 }
 
 function createTodo(value) {
-  createElement(value);
-  localStorage.setItem("todo", JSON.stringify(todoList));
+  let createTodo = { text: value, completed: false };
+  createElement(createTodo);
   input.value = "";
-  console.log(todoList);
 }
+
+function saveTodo() {
+  let todos = [];
+  document.querySelectorAll(".todo").forEach((todo) => {
+    todos.push({
+      text: todo.querySelector("p").innerText,
+      completed: todo.querySelector("input").checked,
+    });
+  });
+  localStorage.setItem("todo", JSON.stringify(todos));
+}
+
+function getTodo() {
+  let localTodo = JSON.parse(localStorage.getItem("todo")) || [];
+  console.log(localTodo);
+
+  localTodo.forEach((todo) => {
+    createElement(todo);
+  });
+}
+
+getTodo();
